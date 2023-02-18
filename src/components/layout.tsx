3120c63773bec4b93a4d771712db75c67a2927d6
J3S5E <67044022+J3S5E@users.noreply.github.com>
2023-02-18 11:28:22 +0800

    about me done

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             </Head>
             <main className="flex flex-col bg-gradient-to-b from-[#210050] to-[#0a0a16] text-white min-h-screen">
                 <Navbar />
-                <div className="flex flex-col items-center justify-center flex-grow">
+                <div className="flex flex-col items-center justify-center flex-grow px-4 py-8">
                     {children}
                 </div>
                 <Footer />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,45 @@
 import { type NextPage } from "next";
 import Link from "next/link";
-
-import { api } from "../utils/api";
+import type { ReactNode } from "react";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Hello, my name is <span className="text-[hsl(125,100%,60%)]">Jesse Marr</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
+          <Paragraph>
+            I am currently in my final year of studying software engineering at Curtin University.
+            While I am currently a systems administrator at a high school, my passion lies in developing software that can solve complex problems and improve people&#39;s lives.
+          </Paragraph>
+          <Paragraph>
+            Throughout my academic journey, I have worked on different <Link className="text-blue-500 underline" href={"/projects"}>projects</Link> that have given me opportunities to learn and grow to increase my <Link className="text-blue-500 underline" href={"/skills_and_tech"}>skillset</Link>.
+          </Paragraph>
+          <Paragraph>
+            My goal is to continue growing as a software engineer and contribute to innovative projects that can make a positive impact on society. I am currently seeking opportunities to apply my skills and knowledge to real-world challenges, and I am open to internships or part-time roles while I complete my studies.
+          </Paragraph>
+          <Paragraph>
+            In my spare time, I enjoy a range of hobbies that keep me balanced and focused. I play golf and find the game to be a great way to unwind and connect with nature. I also enjoy board-sports such as surfing and skateboarding, which require a balance of physical strength, mental focus, and creativity.<br/>
+            I am a gamer and find that video games provide a unique platform for storytelling and problem-solving. I enjoy exploring new games and the technologies that power them, and believe that gaming can have a positive impact on society.
+          </Paragraph>
+          <Paragraph>
+            If you have any opportunities that align with my goals, please feel free to reach out to me through any of <Link className="text-blue-500 underline" href={"/contact"}>these contact methods</Link>.
+          </Paragraph>
+          <Paragraph>
+            Thank you for taking the time to learn more about me, and I look forward to connecting with you.
+          </Paragraph>
         </div>
     </>
+  );
+};
+
+const Paragraph = ({children}: {children: ReactNode}) => {
+  return (
+    <span className="text-xl text-gray-300 w-full text-left">
+      {children}
+    </span>
   );
 };
 
