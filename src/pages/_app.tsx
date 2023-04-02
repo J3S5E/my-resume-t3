895410986885aff5b159abd789916249d786756c
J3S5E <67044022+J3S5E@users.noreply.github.com>
2023-04-02 10:29:58 +0800

    Added NextNProgress for page loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 
 import { api } from "../utils/api";
 
@@ -14,9 +15,32 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Layout>
+        <LoadingBar />
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
+  );
+};
+
+const LoadingBar = () => {
+  return (
+     <NextNProgress
+          color="hsl(125,100%,60%)"
+          stopDelayMs={50}
+          height={1}
+          showOnShallow={false}
+          options={{ easing: "ease", speed: 500 }}
+          // set spinner to hidden
+          transformCSS={(css: string) => {
+            const split = css.split("#nprogress .spinner {");
+            split[1] = `
+              display: none !important;
+              ${split[1] || ""}
+              `;
+            css = split.join("#nprogress .spinner {");
+            return <style>{css}</style>;
+          }}
+        />
   );
 };
 
