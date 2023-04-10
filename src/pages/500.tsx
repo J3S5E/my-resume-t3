3aa0d4fd6b840246339c7c47345806266928571b
J3S5E <67044022+J3S5E@users.noreply.github.com>
2023-04-10 17:20:55 +0800

    Changed custom error pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+
+const ServerSideError: NextPage = () => {
+  return (
+    <>
+      <div className="text-5xl">
+        <b>🙃</b> Server-side error occurred
+      </div>
+    </>
+  );
+};
+
+export default ServerSideError;
