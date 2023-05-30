34bcb1bdbc6eeac0b35705159d29d3c4a16d7510
J3S5E <67044022+J3S5E@users.noreply.github.com>
2023-05-30 17:34:28 +0800

    Updated resume button in nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -147,7 +147,7 @@ const NavResume = () => {
           type="button"
           className="mr-3 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
         >
-          Download Resume
+          Resume
         </button>
       </a>
     </div>
