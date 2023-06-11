9a25612543cb3e1324cd2dfe6b27cfbd30fad45e
J3S5E <67044022+J3S5E@users.noreply.github.com>
2023-06-11 15:29:04 +0800

    Changed Resume file name + button action
    No longer downloads resume when button clicked

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -142,7 +142,7 @@ const NavOption = ({ name, link, triggerHide}: NavOptionProp) => {
 const NavResume = () => {
   return (
     <div className="flex">
-      <a href="/Resume.pdf" download={"Jesse-Marr_Resume.pdf"}>
+      <a href="/JesseMarr-Resume.pdf">
         <button
           type="button"
           className="mr-3 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
